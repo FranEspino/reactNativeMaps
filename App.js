672bf99895f1af9369fe8ea9e530d606a349bd69
franespino <frapo_developer@outlook.com.pe>
2023-09-02 15:02:44 -0500

    Sdk Google maps

---
+++ App.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import MapScreen from './src/screens/MapScreen'
+
+const App = () => {
+  return (
+    <View>
+      <MapScreen/>
+    </View>
+  )
+}
+
+export default App
